11f97de0daec603c80cf717525ca0ee46c1b22f1
nicolasSchinagel <drivepoweringames@gmail.com>
2026-03-06 10:35:14 -0300

    Primeiro Save

---
+++ AplicacaoBanco/Repository/Contract/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using AplicacaoBanco.Models;
+
+namespace AplicacaoBanco.Repository.Contract
+{
+    public interface IUsuarioRepository
+    {
+        //CRUD
+        IEnumerable<Usuario> ObterTodosUsuarios();
+        void Cadastrar(Usuario usuario);
+        void Atualizar(Usuario usuario);
+        Usuario ObterUsuario(int Id);
+        void Excluir(int Id);
+    }
+}
