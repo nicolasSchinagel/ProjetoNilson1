ee3f8c2cf8f0fc3533f3c8543af301a2b3b5cc7a
belafrata <fratabela@gmail.com>
2026-03-18 09:05:17 -0300

    Arrumando erros1

--- AplicacaoBanco/obj/Debug/net8.0/AplicacaoBanco.AssemblyInfo.cs
+++ AplicacaoBanco/obj/Debug/net8.0/AplicacaoBanco.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AplicacaoBanco")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+404a031db0b21a319b6547166bdbab5a22c22dd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a69026b7b8320e65230189d09faa4f03f56b48f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AplicacaoBanco")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AplicacaoBanco")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
