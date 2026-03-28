bff70abccf6ed02804dc5d744cf65b4da3be90a4
nicolasSchinagel <drivepoweringames@gmail.com>
2026-03-28 09:38:58 -0300

    finalizando design

--- AplicacaoBanco/obj/Debug/net8.0/AplicacaoBanco.AssemblyInfo.cs
+++ AplicacaoBanco/obj/Debug/net8.0/AplicacaoBanco.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AplicacaoBanco")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6c56fa4cb270ade1487f7e39a27d2beb648b22e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73bc74789d9d79c78050f1a8549ba9ef3d5fd895")]
 [assembly: System.Reflection.AssemblyProductAttribute("AplicacaoBanco")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AplicacaoBanco")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
